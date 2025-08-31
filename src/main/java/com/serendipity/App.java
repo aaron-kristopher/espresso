5fd922d3508974e7edb0953c607f503e3233028b
Aaron Lim <aaron.lim.cstr@gmail.com>
2025-08-31 17:36:30 +0800

    first commit

---
+++ src/main/java/com/serendipity/App.java
@@ -0,0 +1,67 @@
+package com.serendipity;
+
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.*;
+
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) {
+        Terminal terminal = null;
+
+        try {
+            terminal = new DefaultTerminalFactory().createTerminal();
+            final TextGraphics textGraphics = terminal.newTextGraphics();
+
+            // Open a new buffer just like in vim
+            terminal.enterPrivateMode();
+            int cursorX = 0;
+            int cursorY = 0;
+
+            KeyStroke keyStroke = terminal.readInput();
+            StringBuffer content = new StringBuffer();
+
+            while (keyStroke.getKeyType() != KeyType.Escape) {
+                
+                switch (keyStroke.getKeyType()) {
+                    case ArrowUp:
+                        cursorY = Math.max(cursorY - 1, 0);
+                        break;
+                    case ArrowDown:
+                        cursorY++;
+                        break;
+                    case ArrowLeft:
+                        cursorX = Math.max(cursorX - 1, 0);
+                        break;
+                    case ArrowRight:
+                        cursorX++;
+                        break;
+                    default:
+                        content.append(keyStroke.getCharacter());
+                        break;
+                }
+
+                terminal.setCursorPosition(cursorX, cursorY);
+                textGraphics.putString(0, 0, content.toString());
+
+                terminal.flush();
+                keyStroke = terminal.readInput();
+            }
+
+        } catch (Exception e){
+            System.err.println("Unexpected error occured: " + e.getMessage());
+        } finally {
+            if (terminal != null) {
+                try {
+                    terminal.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
