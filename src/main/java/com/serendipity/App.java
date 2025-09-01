6fc4a3b6d37292670e4dfc2eeaf277cd09229ed8
Aaron Lim <aaron.lim.cstr@gmail.com>
2025-09-01 21:41:04 +0800

    feat: Enable moving of buffer based on cursor index

--- src/main/java/com/serendipity/App.java
+++ src/main/java/com/serendipity/App.java
@@ -9,59 +9,88 @@ import com.googlecode.lanterna.*;
 
 import java.io.*;
 
+import com.serendipity.buffer.GapBuffer;
+
 public class App {
+
+
     public static void main(String[] args) {
-        Terminal terminal = null;
-
-        try {
-            terminal = new DefaultTerminalFactory().createTerminal();
-            final TextGraphics textGraphics = terminal.newTextGraphics();
-
-            // Open a new buffer just like in vim
-            terminal.enterPrivateMode();
-            int cursorX = 0;
-            int cursorY = 0;
-
-            KeyStroke keyStroke = terminal.readInput();
-            StringBuffer content = new StringBuffer();
-
-            while (keyStroke.getKeyType() != KeyType.Escape) {
-                
-                switch (keyStroke.getKeyType()) {
-                    case ArrowUp:
-                        cursorY = Math.max(cursorY - 1, 0);
-                        break;
-                    case ArrowDown:
-                        cursorY++;
-                        break;
-                    case ArrowLeft:
-                        cursorX = Math.max(cursorX - 1, 0);
-                        break;
-                    case ArrowRight:
-                        cursorX++;
-                        break;
-                    default:
-                        content.append(keyStroke.getCharacter());
-                        break;
-                }
-
-                terminal.setCursorPosition(cursorX, cursorY);
-                textGraphics.putString(0, 0, content.toString());
-
-                terminal.flush();
-                keyStroke = terminal.readInput();
-            }
-
-        } catch (Exception e){
-            System.err.println("Unexpected error occured: " + e.getMessage());
-        } finally {
-            if (terminal != null) {
-                try {
-                    terminal.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        GapBuffer gb = new GapBuffer(25);
+        gb.insertChar('H');
+        gb.insertChar('e');
+        gb.insertChar('l');
+        gb.insertChar('l');
+        gb.insertChar('o');
+        gb.insertChar(' ');
+        gb.insertChar('W');
+        gb.insertChar('o');
+        gb.insertChar('r');
+        gb.insertChar('l');
+        gb.insertChar('d');
+        gb.insertChar('!');
+        gb.moveGapTo(6);
+        gb.insertChar('m');
+        gb.insertChar('y');
+        gb.insertChar(' ');
+        gb.moveGapTo(24);
+        gb.insertChar('?');
+        gb.insertChar('!');
+        gb.insertChar('?');
+        System.out.println(gb);
     }
+
+    // public static void main(String[] args) {
+    //     Terminal terminal = null;
+    //
+    //     try {
+    //         terminal = new DefaultTerminalFactory().createTerminal();
+    //         final TextGraphics textGraphics = terminal.newTextGraphics();
+    //
+    //         // Open a new buffer just like in vim
+    //         terminal.enterPrivateMode();
+    //         int cursorX = 0;
+    //         int cursorY = 0;
+    //
+    //         KeyStroke keyStroke = terminal.readInput();
+    //         StringBuffer content = new StringBuffer();
+    //
+    //         while (keyStroke.getKeyType() != KeyType.Escape) {
+    //
+    //             switch (keyStroke.getKeyType()) {
+    //                 case ArrowUp:
+    //                     cursorY = Math.max(cursorY - 1, 0);
+    //                     break;
+    //                 case ArrowDown:
+    //                     cursorY++;
+    //                     break;
+    //                 case ArrowLeft:
+    //                     cursorX = Math.max(cursorX - 1, 0);
+    //                     break;
+    //                 case ArrowRight:
+    //                     cursorX++;
+    //                     break;
+    //                 default:
+    //                     content.append(keyStroke.getCharacter());
+    //                     break;
+    //             }
+    //
+    //             terminal.setCursorPosition(cursorX, cursorY);
+    //             textGraphics.putString(0, 0, content.toString());
+    //
+    //             terminal.flush();
+    //             keyStroke = terminal.readInput();
+    //         }
+    //
+    //     } catch (Exception e){
+    //         System.err.println("Unexpected error occured: " + e.getMessage());
+    //     } finally {
+    //         if (terminal != null) {
+    //             try {
+    //                 terminal.close();
+    //             } catch (IOException e) {
+    //                 e.printStackTrace();
+    //             }
+    //         }
+    //     }
+    // }
 }
--- src/main/java/com/serendipity/buffer/GapBuffer.java
+++ src/main/java/com/serendipity/buffer/GapBuffer.java
@@ -1,5 +1,70 @@
 package com.serendipity.buffer;
 
+import java.util.Arrays;
+
 public class GapBuffer {
+
+    private int start;
+    private int end;
+    private char[] buffer;
+
+    public GapBuffer(int size) {
+        this.start = 0;
+        this.end = size;
+        this.buffer = new char[size];
+    }
+
+    // Initialize buffer with a default size 1024 if not provided
+    public GapBuffer() {
+        this(1024);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < this.buffer.length; i++) {
+            if (this.start <= i && i < this.end) {
+                sb.append('_');
+            } else {
+                sb.append(this.buffer[i]);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public void insertChar(char c) {
+        this.buffer[this.start++] = c;
+    }
+
+    public void moveGapTo(int cursor) {
+
+        if (cursor < this.start) {
+            int length = this.start - cursor;
+            shiftGapToRight(cursor, length);
+        }
+
+        else if (cursor > this.start) {
+            int length = cursor - this.end;
+            shiftGapToLeft(cursor, length);
+        }
+    }
+
+    /**
+     *
+     *
+     */
+    private void shiftGapToRight(int cursor, int length) {
+        while (length-- > 0) {
+            this.buffer[--this.end] = this.buffer[--this.start];
+        }
+    }
+
+    private void shiftGapToLeft(int cursor, int length) {
+        while (length-- > 0) {
+            this.buffer[this.start++] = this.buffer[this.end++];
+        }
+    }
 
 }
