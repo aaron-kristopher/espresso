ab86f35c797df7bbd11b5694d6e4cb5389f6d0c5
Aaron Lim <aaron.lim.cstr@gmail.com>
2025-09-01 22:15:18 +0800

    feat: Enable automatic resizing of buffer when there is no more gap. Fix left and right logical error

--- src/main/java/com/serendipity/buffer/GapBuffer.java
+++ src/main/java/com/serendipity/buffer/GapBuffer.java
@@ -35,6 +35,10 @@ public class GapBuffer {
     }
 
     public void insertChar(char c) {
+        // Grow gapBuffer size if buffer there is no more buffer
+        if (this.start == this.end)
+            this.resize();
+
         this.buffer[this.start++] = c;
     }
 
@@ -42,29 +46,42 @@ public class GapBuffer {
 
         if (cursor < this.start) {
             int length = this.start - cursor;
-            shiftGapToRight(cursor, length);
+            shiftGapToLeft(cursor, length);
         }
 
         else if (cursor > this.start) {
             int length = cursor - this.end;
-            shiftGapToLeft(cursor, length);
-        }
-    }
-
-    /**
-     *
-     *
-     */
-    private void shiftGapToRight(int cursor, int length) {
-        while (length-- > 0) {
-            this.buffer[--this.end] = this.buffer[--this.start];
+            shiftGapToRight(cursor, length);
         }
     }
 
     private void shiftGapToLeft(int cursor, int length) {
         while (length-- > 0) {
+            this.buffer[--this.end] = this.buffer[--this.start];
+        }
+    }
+
+    private void shiftGapToRight(int cursor, int length) {
+        while (length-- > 0) {
             this.buffer[this.start++] = this.buffer[this.end++];
         }
+    }
+
+    private void resize() {
+        int newSize = this.buffer.length * 2;
+        char[] newBuffer = new char[newSize];
+
+        int leftSize = this.start;
+        int rightSize = this.buffer.length - this.end;
+        int gapSize = newSize - (leftSize + rightSize);
+
+        System.arraycopy(this.buffer, 0, newBuffer, 0, leftSize);
+        System.arraycopy(this.buffer, this.end, newBuffer, newSize - rightSize, rightSize);
+
+        // Update pointers
+        this.start = leftSize;
+        this.end = newSize - rightSize;
+        this.buffer = newBuffer;
     }
 
 }
