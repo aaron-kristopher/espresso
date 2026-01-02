77704f02c71c50b224c96a958d72b7375726606d
aaron-kristopher <aaron.lim.cstr@gmail.com>
2026-01-02 17:41:02 +0800

    fix: Fix merge conflict

--- src/main/java/com/serendipity/App.java
+++ src/main/java/com/serendipity/App.java
@@ -9,14 +9,12 @@ import com.googlecode.lanterna.*;
 
 import java.io.*;
 
-<<<<<<< HEAD
 public class App {
 =======
 import com.serendipity.buffer.GapBuffer;
 
 public class App {
 
->>>>>>> feat/gap-buffer
     public static void main(String[] args) {
         Terminal terminal = null;
 
@@ -33,11 +31,6 @@ public class App {
             StringBuffer content = new StringBuffer();
 
             while (keyStroke.getKeyType() != KeyType.Escape) {
-<<<<<<< HEAD
-                
-=======
-
->>>>>>> feat/gap-buffer
                 switch (keyStroke.getKeyType()) {
                     case ArrowUp:
                         cursorY = Math.max(cursorY - 1, 0);
